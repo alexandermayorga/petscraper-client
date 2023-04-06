e15879ed502c930cee15118bcf982528e8aa8322
Alexander Mayorga <alexandermayorga@users.noreply.github.com>
2023-04-06 17:08:05 -0500

    Just Prettier formating

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,43 +1,38 @@
-import React,{ KeyboardEvent } from 'react'
+import React, { KeyboardEvent } from "react";
 
 type SearchBoxProps = {
-    onSearch: Function,
-    inputText:string,
-    onChange: Function
-}
+  onSearch: Function;
+  inputText: string;
+  onChange: Function;
+};
 
-export default function SearchBox(props:SearchBoxProps) {
-    const {onSearch,inputText,onChange} = props;
+export default function SearchBox(props: SearchBoxProps) {
+  const { onSearch, inputText, onChange } = props;
 
-    const handleKeyPress = (e:KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter") onSearch(inputText);
-    }
+  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") onSearch(inputText);
+  };
 
-    const handleClick = () =>{
-        onSearch(inputText)
-    }
+  const handleClick = () => {
+    onSearch(inputText);
+  };
 
-    return (
-        <div className="input-group">
-            <input 
-            className="form-control" 
-            type="text" 
-            placeholder="Search by Breed"
-            aria-label="Search by Breed"
-            value={inputText}
-            onChange={(e)=>{onChange(e.target.value)}}
-            onKeyUp={handleKeyPress} //Deprecated
-            />
-            <div className="input-group-append">
-                <button 
-                type="button"
-                className="btn btn-primary" 
-                onClick={handleClick}
-                >
-                    Search
-                </button>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="input-group">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search by Breed"
+        aria-label="Search by Breed"
+        value={inputText}
+        onChange={(e) => {
+          onChange(e.target.value);
+        }}
+        onKeyUp={handleKeyPress}
+      />
+      <button className="btn btn-primary" type="button" onClick={handleClick}>
+        Search
+      </button>
+    </div>
+  );
 }
